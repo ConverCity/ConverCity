fcecafaabd998ed70816e07048dce08d37878f8d
salaback <sean.alaback@gmail.com>
2015-03-08 17:55:19 -0400

    add homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,13 +33,39 @@
 			.quote {
 				font-size: 24px;
 			}
+
+			.btn {
+				  display: inline-block;
+				  margin-bottom: 0;
+				  font-weight: 300;
+				  text-align: center;
+				  vertical-align: middle;
+				  cursor: pointer;
+				  border: 1px solid transparent;
+				  padding: 6px 12px;
+				  font-size: 14px;
+				  line-height: 1.42857143;
+				  border-radius: 4px;
+				  text-decoration: none;
+				  border: 1px;
+				  border-color: #333333;
+				  color: white;
+				  background-color: gray;
+				  border-color: #cccccc;
+			}
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<img src="/conversitylogo.png">
+				<br>
+				<br>
+				<div class="quote">To get started text (617) 545-3311 "I'm afraid of my husband"</div>
+
+				<br>
+				<br>
+				<a class="btn" href="/question-tree">View Question Tree</a>
 			</div>
 		</div>
 	</body>
